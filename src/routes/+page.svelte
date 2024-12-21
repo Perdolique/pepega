1dee6325b1c71f5e063d128c4ace59e53c0300e4
Pier Dolique <161577745+Perdolique@users.noreply.github.com>
2024-12-21 23:03:07 +0200

    Update src/routes/+page.svelte
    Co-authored-by: Roman Nuritdinov <github@woof.slmail.me>

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,10 +1,2 @@
 <h1>Welcome to Pepega</h1>
 <p>Visit <a href="https://github.com/Perdolique/pepega">Perdolique/pepega</a> to read the documentation</p>
-
-<script lang="ts">
-  import { onMount } from 'svelte';
-  
-  onMount(() => {
-    console.log('Hello from +page.svelte');
-  });
-</script>
